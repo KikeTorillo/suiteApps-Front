9d9537d23d0c39173a60e9a630be5062e6efc95d
kike <arellanestorillo@yahoo.com>
2025-01-03 20:36:08 -0600

    se desactiva minificacion

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    minify: false,
+  },
+  esbuild: {
+    minify: false,
+    minifySyntax: false,
+    minifyIdentifiers: false,
+    minifyWhitespace: false,
+  },
   plugins: [react()],
 })
